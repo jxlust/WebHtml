7c9db0af3ef673d74bd28004251b20be76634264
xunliang jiang <xunliangjiang@xunliangdeMacBook-Pro.local>
2022-06-08 22:36:56 +0800

    feat: event loop

--- JS相关知识点/js事件循环.js
+++ JS相关知识点/js事件循环.js
@@ -5,15 +5,32 @@ setTimeout(() => {
         resolve()
     }).then(() => {
         console.log(8);
+        return 22
+    }).then((data) => {
+        console.log(data);
     })
     console.log(2);
-}, 10);
+}, 0);
 
 new Promise(resolve => {
     console.log(3);
-    resolve();
+    setTimeout(() => {
+        resolve();
+        console.log(999)
+    }, 0);
+    console.log(333);
 }).then(res => {
     console.log(4);
 })
 
 console.log(6);
+
+// console.log(1)
+
+// setTimeout(() => {
+//     console.log(2)
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log(3)
+// }, 100);
