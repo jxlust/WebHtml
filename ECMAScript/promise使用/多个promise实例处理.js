3431f9c6e1b90adc60b3754806e86d74a7c4ae53
jxlust <jxljxufe@163.com>
2023-09-19 11:00:37 +0800

    feat: css text

--- ECMAScript/promise使用/多个promise实例处理.js
+++ ECMAScript/promise使用/多个promise实例处理.js
@@ -1,41 +1,57 @@
 function p1(data) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(`${data}-p1-success`)
+      resolve(`${data}-p1-success`);
     }, 5000);
-  })
+  });
 }
 async function p2(data) {
   // let ret = await `${data}-p2-success`;
-  let ret = await Promise.resolve(`${data}-p2-success`)
+  let ret = await Promise.resolve(`${data}-p2-success`);
 
   return ret;
 }
 function p3(data) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(`${data}-p3-success`)
+      resolve(`${data}-p3-success`);
     }, 6000);
-  })
+  });
 }
 async function get() {
-  let promises = [p1('xiaomi'), p2('iphone'),p3('opop')];
+  let promises = [p1("xiaomi"), p2("iphone"), p3("opop")];
   let ret = [];
   for (let p of promises) {
     console.log(1);
     let r = await p;
-    console.log(1,r);
+    console.log(1, r);
     ret.push(r);
   }
   console.log(ret);
   return ret;
 }
 async function get2() {
-  let promises = [p1('xiaomi'), p2('iphone'),p3('opop')];
+  let promises = [p1("xiaomi"), p2("iphone"), p3("opop")];
   let ret = await Promise.all(promises);
-  console.log('ret:',ret);
+  console.log("ret:", ret);
 }
 // get().then(ret => {
 //   console.log('回调：', ret);
 // })
-get2()
+// get2();
+
+async function test() {
+  let ok = await p1("xxxxx");
+  // Promise.resolve("ok:" + ok);
+  // return "return ok:" + ok;
+  // Promise.reject("hhhhh" + ok);
+  throw new Error("error:" + ok);
+}
+
+test()
+  .then((data) => {
+    console.log("test data:", data);
+  })
+  .catch((e) => {
+    console.log("e:", e);
+  });
